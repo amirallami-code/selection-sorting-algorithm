4c768b2448e1f2bb71583d1c8689d8db6b8d2cf6
amirhossein allami <captain2480722023@gmail.com>
2023-07-24 23:58:27 +0330

    Update

--- js/app.js
+++ js/app.js
@@ -7,10 +7,14 @@ const afterWrapper = $.querySelector('.After')
 
 let numbers = []
 
+let smallestNum = numbers[0]
+    , smallestIndex = 0
+    , findSmallestNumber = null
+
 inputElem.addEventListener('keypress', event => {
 
     if (event.key === 'Enter') {
-        numbers.push(input.value)
+        numbers.push(Number(input.value))
         input.value = ''
         beforeWrapper.innerHTML = ''
         for (let item of numbers) {
@@ -19,6 +23,40 @@ inputElem.addEventListener('keypress', event => {
     }
 })
 
+const selectionSort = arr => {
+
+    let smallestNum = arr[0]
+    let smallestIndex = 0
+
+    for (let i = 0; i != arr.length; i++) {
+        findSmallestNumber = findSmallest(arr, smallestNum, smallestIndex)
+    }
+}
+
+const findSmallest = arr => {
+
+    for (let i = 0; i != arr.length; i++) {
+        if (arr[i] <= smallestNum) {
+            smallestNum = arr[i]
+            smallestIndex = i
+            console.log('this is smallest number', smallestNum);
+            console.log('this is smallest number index', smallestIndex);
+        }
+    }
+}
+
+const afterGenerator = newArr => {
+
+    afterWrapper.innerHTML = ''
+    for (let number of newArr) {
+        afterWrapper.insertAdjacentHTML("beforeend", `<li>${number}</li>`)
+    }
+}
+
+buttonElem.addEventListener("click", () => selectionSort(numbers))
+
+
+/*
 let findSmallest = arr => {
     smallest = arr[0]
     smallestIndex = 0
@@ -35,7 +73,7 @@ let findSmallest = arr => {
 let selectionSort = arr => {
     newArr = []
 
-    for (let i = 0; i !== arr.length; i++) {
+    for (let i = 0; i != arr.length; i++) {
         smallest = findSmallest(arr)
         newArr.push(arr[smallest])
         arr.splice(smallest, 1)
@@ -43,12 +81,4 @@ let selectionSort = arr => {
     afterGenerator(newArr)
 }
 
-const afterGenerator = newArr => {
-
-    afterWrapper.innerHTML = ''
-    for (let number of newArr) {
-        afterWrapper.insertAdjacentHTML("beforeend", `<li>${number}</li>`)
-    }
-}
-
-buttonElem.addEventListener("click", () => selectionSort(numbers))
+*/
